87926a44c5045a0ecc827d553446ea8022f7c0b0
christiaanjohns <christiaanjohns@gmail.com>
2018-12-13 11:54:58 -0700

    Refactor logic of extracting pieces of the string. Added head and encodeDigits methods.

--- Soundex.cpp
+++ Soundex.cpp
@@ -10,19 +10,8 @@
      */
 string Soundex::zeroPad(const string& word) const
 {
-    /*if(word.size() == 3)
-    {
-        return word + "0";
-    }
-    else if(word.size() == 2)
-    {
-        return word + "00";
-    }
-    else if(word.size() == 1)
-    {
-        return word + "000";
-    } else return word;*/
-    return word + "000";
+    auto zerosNeeded = 4 - word.length();
+    return word + string(zerosNeeded, '0');
 }
 /**
     * @brief Encode string to Soundex Algorithm format
@@ -31,5 +20,30 @@ string Soundex::zeroPad(const string& word) const
     */
 string Soundex::encode(const string& word) const
 {
-    return zeroPad(word);
+    return zeroPad(head(word) + encodeDigits(word));
+}
+/**
+ * @brief - Extract the first char of the string
+ * @param word - input string
+ * @return - first letter of string
+ */
+string Soundex::head(const string &word) const
+{
+    return word.substr(0,1);
+}
+/**
+ * @brief - Encode the string into digits using Soundex Algorithm rules
+ * @param word - input string
+ * @return - encoded string
+ */
+string Soundex::encodeDigits(const string &word) const
+{
+    if(word.length() > 1)
+    {
+        return "1";
+    }
+    else
+    {
+        return "";
+    }
 }
--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -40,3 +40,8 @@ TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
     ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
 
+TEST_F(SoundexEncoding, ReplaceConsonantsWithAppropriateDigits)
+{
+    ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
+}
+
