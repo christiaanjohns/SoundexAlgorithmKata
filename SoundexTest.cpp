0afb99bd3cef0478a8b8cad51fdd40f4c09b92ac
christiaanjohns <christiaanjohns@gmail.com>
2018-12-13 11:07:46 -0700

    Ensure pads with zeros to 4 digits

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -4,10 +4,36 @@ using namespace std;
 
 class Soundex
 {
+    /**
+     * @brief pad word to 4 digits
+     * @param word - input string
+     * @return - padded string
+     */
+    string zeroPad(const string& word) const
+    {
+        /*if(word.size() == 3)
+        {
+            return word + "0";
+        }
+        else if(word.size() == 2)
+        {
+            return word + "00";
+        }
+        else if(word.size() == 1)
+        {
+            return word + "000";
+        } else return word;*/
+        return word + "000";
+    }
 public:
+    /**
+     * @brief Encode string to Soundex Algorithm format
+     * @param word - string to code
+     * @return - encoded string
+     */
     string encode(const string& word) const
     {
-        return word;
+        return zeroPad(word);
     }
 };
 
@@ -27,6 +53,8 @@ public:
    If you have more than 3 letters, just retain the first 3 numbers.
 */
 
+
+//TESTS
 #include "gmock/gmock.h"
 using::testing::Eq;
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
@@ -36,6 +64,16 @@ TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
    // Act
    auto encode = soundex.encode("A");
    // Assert
-   ASSERT_THAT(encode, Eq("A"));
+   ASSERT_THAT(encode, Eq("A000"));
+}
+
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
+{
+    // Arrange
+    Soundex soundex;
+    // Act
+    auto encode = soundex.encode("I");
+    // Assert
+    ASSERT_THAT(encode, Eq("I000"));
 }
 
